21ca2d030a3e629af60c971dffb060c687d8e0f8
Diego Monteiro <damonteiro89@gmail.com>
2022-07-09 13:25:59 -0300

    Media do aluno.

---
+++ src/MediaAluno.java
@@ -0,0 +1,12 @@
+public class MediaAluno {
+
+    public static void main(String[] args) {
+            int notaSemestre1 = 6;
+            int notaSemestre2 = 8;
+
+            int MediaAluno = (notaSemestre1 + notaSemestre2) / 2;
+            System.out.println(("Media do Aluno: ") + MediaAluno);
+
+    }
+
+}
